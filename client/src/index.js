4678ff1577b10e24e36a68102a01d4d4a47ed53c
Daniel <daniel@stoutlabs.com>
2018-04-14 02:10:55 -0400

    Removed a couple quick debug/log lines

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,6 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 import axios from "axios";
-window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -19,6 +18,3 @@ ReactDOM.render(
   </Provider>,
   document.querySelector("#root")
 );
-
-console.log("stripe key is:", process.env.REACT_APP_STRIPE_KEY);
-console.log("Environment is:", process.env.NODE_ENV);
