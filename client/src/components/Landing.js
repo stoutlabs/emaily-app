43147789b49004fc3fb24213c51dbcb7907c95d4
Daniel <daniel@stoutlabs.com>
2018-07-25 02:12:07 -0400

    Edit to landing message...lol

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,8 +4,12 @@ const Landing = () => {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Emaily!</h1>
-      <h2>Shiiiieeeet, mang.</h2>
-      <p>Collect feedback and whatnot.</p>
+      <h2>Pardon the mess... about 2 total minutes were spent on styles.</h2>
+      <p>
+        Emaily<sup>&trade;</sup> lets you create surveys to collect feedback from your clients.
+        Simply add credits via the button above, create your survey(s), import your email list(s),
+        and hit 'send'!
+      </p>
     </div>
   );
 };
